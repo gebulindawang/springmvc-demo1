22a066bd96d56f924ea952cc03ea5e4df944e7e6
超级无敌哥布林大王 <15176388+super-invincible-goblin-king@user.noreply.gitee.com>
2025-09-22 21:36:56 +0800

    完成登录功能

--- src/main/java/com/hmall/springmvcdemo/SpringmvcDemoApplication.java
+++ src/main/java/com/hmall/springmvcdemo/SpringmvcDemoApplication.java
@@ -9,5 +9,8 @@ public class SpringmvcDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringmvcDemoApplication.class, args);
     }
-
+    public String login(String username, String password) {
+        System.out.println("登录成功");
+        return "login success";
+    }
 }
