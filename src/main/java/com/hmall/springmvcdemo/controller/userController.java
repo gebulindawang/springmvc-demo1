30f715bd144c7caa2d13e657458b91ecb1e104ad
超级无敌哥布林大王 <15176388+super-invincible-goblin-king@user.noreply.gitee.com>
2025-09-22 22:07:24 +0800

    初始化

--- src/main/java/com/hmall/springmvcdemo/controller/userController.java
+++ src/main/java/com/hmall/springmvcdemo/controller/userController.java
@@ -2,17 +2,5 @@ package com.hmall.springmvcdemo.controller;
 
 public class userController {
 
-    public String login(String username, String password) {
-        return "success";
-    }
 
-    public void logout() {
-    }
-
-    public void sayHello() {
-
-    }
-    public void selectUser(){
-
-    }
 }
